c2b6e3f566c4edface652cdc1b888fa406f59dcc
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-02-22 23:39:24 +0100

    add mock data for Forecast testing

--- src/containers/Forecast/Forecast.js
+++ src/containers/Forecast/Forecast.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 import CityInput from '../CityInput/CityInput';
 import WeatherNow from '../WeatherNow/WeatherNow';
 import WeatherForecast from '../WeatherForecast/WeatherForecast';
-import axios from 'axios';
+
+// import axios from 'axios';
+            import { forecastData, weatherData } from '../../weatherData';
 
 import styles from './Forecast.module.css'
 
@@ -19,40 +21,48 @@ class Forecast extends Component {
     apiID = 'fd8de74bb30e63fa5bdd052cabd79493'
 
     getWeatherData = (city) => {
-        const query = `?q=${city}&APPID=${this.apiID}`
+        
+        // const query = `?q=${city}&APPID=${this.apiID}`
+        // axios.all([
+        //     axios.get(`${this.url}weather${query}`),
+        //     axios.get(`${this.url}forecast${query}`)
+        //     ])
+        //     .then(([weather, forecast]) => {
+        //         console.log(weather.data)
+        //         console.log(forecast.data.list)
+        //         this.setState({
+        //             weatherNow: weather.data,
+        //             weatherForecast: forecast.data.list,
+        //             city: city
+        //         })
+        //     })
+        //     .catch(error => {
+        //         this.setState({errorMessage: error.response.data.message})
+        //     })
 
-        axios.all([
-            axios.get(`${this.url}weather${query}`),
-            axios.get(`${this.url}forecast${query}`)
-            ])
-            .then(([weather, forecast]) => {
-                this.setState({
-                    weatherNow: weather.data,
-                    weatherForecast: forecast.data.list,
-                    city: city
-                })
-            })
-            .catch(error => {
-                this.setState({errorMessage: error.response.data.message})
-            })
-
+                        this.setState({
+                            weatherNow: weatherData,
+                            weatherForecast: forecastData.list,
+                            city: city
+                        })
     }
 
-    // componentDidMount = () => {
-    // }
-    
+
+    componentDidMount = () => {
+        this.getWeatherData('Krakow')
+    }
+
     render () {
-        
+
         let now = this.state.weatherNow;
         let forecast = this.state.weatherForecast;
         let cityName = this.state.city;
 
         return (
-            <div className={styles.Forecast}>
-                <h1>TEST</h1>
+            <div className={styles.forecast}>
                 <CityInput getWeatherData={this.getWeatherData} />
-                <WeatherNow weatherNow={now} cityName={cityName}/>
-                <WeatherForecast weatherForecast={forecast} cityName={cityName}/>
+                <WeatherNow weatherNow={now} cityName={cityName} />
+                <WeatherForecast weatherForecast={forecast}/>
             </div>
         )
     }
