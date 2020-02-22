f825be290851badf9df11960b08ac9cb297f54f3
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-02-22 21:54:23 +0100

    add components for input and display to Forecast

--- src/containers/Forecast/Forecast.js
+++ src/containers/Forecast/Forecast.js
@@ -1,59 +1,58 @@
 import React, {Component} from 'react';
 
+import CityInput from '../CityInput/CityInput';
+import WeatherNow from '../WeatherNow/WeatherNow';
+import WeatherForecast from '../WeatherForecast/WeatherForecast';
 import axios from 'axios';
 
+import styles from './Forecast.module.css'
+
 class Forecast extends Component {
+    state = {
+        weatherNow: false,
+        weatherForecast: false,
+        errorMessage: '',
+        city: ''
+    }
 
     url = 'http://api.openweathermap.org/data/2.5/'
     apiID = 'fd8de74bb30e63fa5bdd052cabd79493'
 
-    displayWeatherNow = (data) => {
-        console.log(data)
-        // const ids = ['name', 'coords', 'description', 'humidity', 'pressure', 'windspeed', 'winddeg', 'temp', 'icon'];
-        // const disp = {};
+    getWeatherData = (city) => {
+        const query = `?q=${city}&APPID=${this.apiID}`
 
-        // ids.forEach(id => {
-        //     disp[id] = document.querySelector('#' + id)
-        // })
-
-        // disp.name.innerText = `${data.name} `;
-        // let country = document.createElement('span');
-        // country.innerText = ` ( ${data.sys.country} ) `;
-        // country.setAttribute("id", "country");
-        // disp.name.appendChild(country);
-
-        // disp.coords.innerText = `Latitide: ${data.coord.lat}, Longitude: ${data.coord.lon}`;
-        // disp.description.innerText = `Now: ${data.weather[0].description}`;
-        // disp.humidity.innerText = `Humidity: ${data.main.humidity} % `;
-        // disp.pressure.innerText = `Pressure: ${data.main.pressure} hPa`;
-        // disp.windspeed.innerText = `Wind speed: ${data.wind.speed} km/h`;
-        // disp.winddeg.innerText = `Wind direction: ${data.wind.deg} deg`;
-        // disp.temp.innerText = `Temperature: \n${(Number(data.main.temp) - 273.15).toFixed(1)} C | ${(Number(data.main.temp)*9/5-459.67).toFixed(1)} F`;
-
-        // icon.src = `${iconUrl}${data.weather[0].icon}.png`
-        // icon.style.display = 'block';
-
-        // document.querySelector('#weather-now').style.display = 'block'
-
-    }
-    
-
-    componentDidMount = () => {
-        
-        const query = `?q=Krakow&APPID=${this.apiID}`
-        axios.get(`${this.url}weather${query}`)
-            .then(response => {
-                this.displayWeatherNow(response.data)
+        axios.all([
+            axios.get(`${this.url}weather${query}`),
+            axios.get(`${this.url}forecast${query}`)
+            ])
+            .then(([weather, forecast]) => {
+                this.setState({
+                    weatherNow: weather.data,
+                    weatherForecast: forecast.data.list,
+                    city: city
+                })
             })
             .catch(error => {
-                console.log('ERROR:', error);
+                this.setState({errorMessage: error.response.data.message})
             })
 
     }
+
+    // componentDidMount = () => {
+    // }
+    
     render () {
+        
+        let now = this.state.weatherNow;
+        let forecast = this.state.weatherForecast;
+        let cityName = this.state.city;
+
         return (
-            <div className="Forecast">
+            <div className={styles.Forecast}>
                 <h1>TEST</h1>
+                <CityInput getWeatherData={this.getWeatherData} />
+                <WeatherNow weatherNow={now} cityName={cityName}/>
+                <WeatherForecast weatherForecast={forecast} cityName={cityName}/>
             </div>
         )
     }
