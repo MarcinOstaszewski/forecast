20f6dd2d826dfbc1bb9a2d3881a6107935acf923
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-02-22 23:36:28 +0100

    improve WeatherForecast logics and styles

--- src/containers/WeatherForecast/WeatherForecast.js
+++ src/containers/WeatherForecast/WeatherForecast.js
@@ -4,19 +4,36 @@ import styles from './WeatherForecast.module.css'
 
 
 class WeatherForecast extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { data: this.props.data }
-    }
 
+    iconUrl = 'http://openweathermap.org/img/w/'
 
     render() { 
-        return ( 
-            <section id="weather-forecast">
-                <span className={styles.txtBig}>Forecast:</span>
-                <div className={styles.row}>
+
+        let visible = (!(this.props.weatherForecast) ) ? {display: 'none'} : {};
+        // console.log(!(!(this.props.weatherForecast)));
+        let columns = []
+        for (let i = 0; i < 9; i++) { // creates 9 columns with forecast data
+            let data = this.props.WeatherForecast;
+            let column = <div className={styles.nineth}>
                     
-                </div>
+
+
+
+                    {/* <div className={styles.temp}>{(Number(data[i].main.temp) - 273.15).toFixed(1)} C</div>
+                    <div className={styles.icon}>
+                        <img src={this.iconUrl+data[i].weather[0].icon+".png"} alt="weather icon" className={styles.imgSml}></img>
+                    </div>
+                    <div className={styles.temp}>{data[i].dt_txt.slice(5, 10)} {data[i].dt_txt.slice(-8, -3)}</div> */}
+            </div>;
+
+            columns.push(column);
+            
+        }
+        
+        return ( 
+            <section id="weather-forecast" style={visible}>
+                <span className={styles.txtBig}>Forecast:</span>
+                <div className={styles.row}>{columns}</div>
             </section>
          );
     }
