08be8bd41262931809754333bdeac1b8b6708a58
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-02-23 11:00:37 +0100

    improve weatherNow visualisation

--- src/containers/WeatherNow/WeatherNow.js
+++ src/containers/WeatherNow/WeatherNow.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 import styles from './WeatherNow.module.css'
 
 class WeatherNow extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
 
     iconUrl = 'http://openweathermap.org/img/w/'
 
@@ -14,7 +11,7 @@ class WeatherNow extends Component {
         let visible = (!(this.props.weatherNow) ) ? {display: 'none'} : {};
 
         let cityName = this.props.cityName;
-        let coords, description, pressure, humidity, windspeed, winddeg, temp, icon;
+        let coords, description, pressure, humidity, windspeed, winddeg, temp1, temp2, icon;
         if (this.props.weatherNow) {
             coords = `Latitude: ${this.props.weatherNow.coord.lat}, Longitude: ${this.props.weatherNow.coord.lon}`;
             description = `Now: ${this.props.weatherNow.weather[0].description}`;
@@ -22,9 +19,10 @@ class WeatherNow extends Component {
             pressure = `Pressure: ${this.props.weatherNow.main.pressure} hPa`;
             windspeed = `Wind speed: ${this.props.weatherNow.wind.speed} km/h`;
             winddeg = `Wind direction: ${this.props.weatherNow.wind.deg} deg`;
-            temp = `Temperature: \n${(Number(this.props.weatherNow.main.temp) - 273.15).toFixed(1)} C | ${(Number(this.props.weatherNow.main.temp)*9/5-459.67).toFixed(1)} F`;
+            temp1 = `Temperature:`;
+            temp2 = `${(Number(this.props.weatherNow.main.temp) - 273.15).toFixed(1)} C | ${(Number(this.props.weatherNow.main.temp)*9/5-459.67).toFixed(1)} F`;
             
-            icon = <img src={this.iconUrl + this.props.weatherNow.weather[0].icon + '.png'} className={styles.imgBig} alt="weather icon"></img>
+            icon = <img className={styles.imgBig} src={this.iconUrl + this.props.weatherNow.weather[0].icon + '.png'}  alt="weather icon"  />
         }
 
         return ( 
@@ -42,8 +40,8 @@ class WeatherNow extends Component {
                         <div id={styles.winddeg}>{winddeg}</div>
                     </div>
                     <div className={styles.row}>
-                        <div >{icon}</div>
-                        <div id={styles.temp}>{temp}</div>
+                        <div>{icon}</div>
+                        <div id={styles.temp}><span>{temp1}</span><b>{temp2}</b></div>
                     </div>
                 </div>
             </section>
