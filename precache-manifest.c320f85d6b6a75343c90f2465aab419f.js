f9053de4e409f402d3f67def3c5488f67a9377e4
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-02-23 16:00:40 +0100

    Updates

---
+++ precache-manifest.c320f85d6b6a75343c90f2465aab419f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478352fd8a6b2337382e037d41da1895",
+    "url": "/forecast/index.html"
+  },
+  {
+    "revision": "164957a769fafb4615ed",
+    "url": "/forecast/static/css/main.5c816729.chunk.css"
+  },
+  {
+    "revision": "492fdc5fa01ee8359215",
+    "url": "/forecast/static/js/2.56292799.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/forecast/static/js/2.56292799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164957a769fafb4615ed",
+    "url": "/forecast/static/js/main.4ffb6ebc.chunk.js"
+  },
+  {
+    "revision": "a262437d2fa9eb291f4d",
+    "url": "/forecast/static/js/runtime-main.53cf1a6e.js"
+  }
+]);
